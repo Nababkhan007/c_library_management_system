0d13338dced3c75f2650c04f1d37a5e68b3f5617
Nababkhan007 <markohenry46@gmail.com>
2023-12-04 23:01:01 +0600

    Add user panel function

--- library_management_system.c
+++ library_management_system.c
@@ -72,13 +72,13 @@ void password(){
     // verifies the password
     if(strcmp(code, pwd) == 0)
     {
-        printf("\nLogin Successful!\n");
+        printf("\nLogin Successful!\nLoading...\n");
         Sleep(2000);
         menu();
     }
     else
     {
-        printf("\nInvalid Password!");
+        printf("\nInvalid Password!\n");
         (passTerminator == 3) ? exit(0) : passTerminator++;
         Sleep(2000);
         password();
@@ -106,7 +106,7 @@ void menu(){
     switch(number)
     {
         case 1:
-            printf("User panel\n");
+            userPanel();
             break;
         case 2:
             printf("Book panel\n");
@@ -117,4 +117,63 @@ void menu(){
             menu();
     }
 }
+
+// User Panel sub-functions
+void userPanel(){
+
+    system("cls");
+    fflush(stdin);
+
+    int number;
+
+    printf("-----------------------------------------------\n");
+    printf(">>> Library Management System - User Panel <<< \n");
+    printf("-----------------------------------------------\n\n");
+    printf("> 1. Add User \n");
+    printf("> 2. Modify User \n");
+    printf("> 3. List User \n");
+    printf("> 4. List Rentals \n");
+    printf("> 5. Search User \n");
+    printf("> 6. Delete User \n");
+    printf("> 7. Open Main Menu \n");
+    printf("> 8. Close the Program... \n\n");
+
+    printf("> Enter the number & hit ENTER: ");
+    scanf("%d", &number);
+
+    fflush(stdin);
+
+    switch(number)
+    {
+        case 1:
+            printf("Add User\n");
+            break;
+        case 2:
+            printf("Modify User\n");
+            break;
+        case 3:
+            printf("List User\n");
+            break;
+        case 4:
+            printf("Rent List\n");
+            break;
+        case 5:
+            printf("Search User\n");
+            break;
+        case 6:
+            printf("Delete User\n");
+            break;
+        case 7:
+            menu();
+            break;
+        case 8:
+            printf("End Screen\n");
+            break;
+        default:
+            printf("Invalid Input!");
+            Sleep(2000);
+            userPanel();
+    }
+}
+
 
